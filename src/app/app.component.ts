af6b48579a36ad860528e0e27528d3158c496b45
Angela Mitchell <anglarenmit@gmail.com>
2021-01-15 21:57:25 -0600

    Used RxJS operators to transform response data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   loadedPosts = [];
@@ -19,7 +20,7 @@ export class AppComponent implements OnInit {
     // Send Http request
     this.http
       .post(
-        "https://ng-complete-guide-23686-default-rtdb.firebaseio.com/posts.json",
+        'https://ng-complete-guide-23686-default-rtdb.firebaseio.com/posts.json',
         postData
       )
       .subscribe((responseData) => {
@@ -39,9 +40,21 @@ export class AppComponent implements OnInit {
   private fetchPosts() {
     this.http
       .get(
-        "https://ng-complete-guide-23686-default-rtdb.firebaseio.com/posts.json"
+        'https://ng-complete-guide-23686-default-rtdb.firebaseio.com/posts.json'
+      )
+      .pipe(
+        map((responseData) => {
+          const postsArray = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postsArray;
+        })
       )
       .subscribe((posts) => {
+        // ...
         console.log(posts);
       });
   }
